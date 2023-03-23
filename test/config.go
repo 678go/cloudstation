16ce58d49701a06ba5a6dc4efbb082320f3645a0
ylinyang <ylinyang1228@gmail.com>
2023-03-23 22:05:12 +0800

    [fix]add tencent cloud

--- test/config.go
+++
@@ -1,7 +0,0 @@
-package test
-
-var (
-	SecretId  = "AKIDTs9LQI0kOo8SiDWq5DE9tVv75pLdgU2b"
-	SecretKey = "Y8Ym1CVhntrmdUF7sRnzg4LGDGlJhsse"
-	BucketUrl = "https://test-1301126197.cos.ap-nanjing.myqcloud.com"
-)
